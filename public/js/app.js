42c8ea1c9a0bdfc9626b6f0dbaea24e12a853929
Diego Dario <diegodario88@gmail.com>
2019-09-20 16:40:49 -0300

    small fixes

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,24 @@
+//Função Scroll
+function scrollAppear()
+{
+    var textoColeta = document.querySelector('.intro-text')
+    var introPosition = textoColeta.getBoundingClientRect().top
+    var screenPosition = window.innerHeight / 2;
+
+
+
+    if (introPosition < screenPosition)
+    {
+        textoColeta.classList.add('intro-appear')
+    } else
+    {
+        textoColeta.classList.remove('intro-appear')
+    }
+
+}
+
+window.addEventListener('scroll', scrollAppear)
+
 //Navigation
 const navSlide = () =>
 {
@@ -232,26 +253,6 @@ switch (dataAtualFormatada())
         break;
 }
 
-//Função Scroll
-function scrollAppear()
-{
-    var textoColeta = document.querySelector('.intro-text')
-    var introPosition = textoColeta.getBoundingClientRect().top
-    var screenPosition = window.innerHeight / 2;
-
-
-
-    if (introPosition < screenPosition)
-    {
-        textoColeta.classList.add('intro-appear')
-    } else
-    {
-        textoColeta.classList.remove('intro-appear')
-    }
-
-}
-
-window.addEventListener('scroll', scrollAppear)
 
 //data no rodapé da página
 const data = new Date();
