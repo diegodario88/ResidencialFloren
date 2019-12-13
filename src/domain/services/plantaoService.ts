448e0205eaec474c8bdf1487ab251022c6f5e60d
Diego Dario <diegodario88@gmail.com>
2019-12-12 22:00:22 -0300

    add gcloud hosting

--- src/domain/services/plantaoService.ts
+++ src/domain/services/plantaoService.ts
@@ -4,7 +4,7 @@ import Plantao from '../entities/Plantao';
 export default class PlantaoService {
     public static async get(): Promise<Plantao> {
         const response = await axios
-            .get('https://florenapp.herokuapp.com/api/v1/plantoes/atual');
+            .get('https://floren-api.appspot.com/api/v1/plantoes/atual');
         return response.data;
     }
 }
